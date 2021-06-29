259d8be99adf9131408aedc083c47a4b107830c7
Zhe Zhao (Zachary) <zhaozhe_lir@hotmail.com>
2021-06-29 18:04:26 +1000

    define days of each month, determin bisextile years(29 days in Feb), and date calculation onwards

--- hello.py
+++ hello.py
@@ -1,3 +1,34 @@
-#!my_env/bin/python3
+#!./my_env/bin/python
 
-print("Hello,World!")
+Days_in_Month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+class Date:
+    def __init__(self, year, month, date):
+        self.year = year
+        self.month = month
+        self.date = date
+
+    def isBisextile(self):
+        return True if self.year%4 == 0 else False
+
+fulldate = Date(1983,8,3)
+
+days = int(input("Enter number of days: \n"))
+
+while days >= 0: 
+    if fulldate.isBisextile():
+        Days_in_Month[1] = 29
+    else:
+        Days_in_Month[1] = 28
+
+    fulldate.date += 1
+
+    if fulldate.date > int(Days_in_Month[fulldate.month-1]):
+        fulldate.month += 1
+        if fulldate.month > 12:
+            fulldate.year += 1
+            fulldate.month = 1
+        fulldate.date = 1
+    days -= 1
+
+print("{}/{}/{}".format(fulldate.date,fulldate.month,fulldate.year))
