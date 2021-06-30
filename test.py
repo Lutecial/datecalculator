72d93fb0e7782afdb92500e11c80f5951efedda8
Zhe Zhao (Zachary) <zhaozhe_lir@hotmail.com>
2021-06-30 22:29:30 +1000

    finallise coding and modules

--- test.py
+++ test.py
@@ -2,8 +2,29 @@
 
 from datediff import Date, diffCalculation
 
-# test unit
+# test unit set 1, should return "19"
 date1 = Date(2, 6, 1983)
 date2 = Date(22, 6, 1983)
-print(diffCalculation(date1, date2), "days")
+if diffCalculation(date1, date2) == 19:
+    print("Pass")
+else:
+    print("error")
+# print(diffCalculation(date1, date2), "days")
 
+# test unit set 2, should return "173"
+date1 = Date(4, 7, 1984)
+date2 = Date(25, 12, 1984)
+if diffCalculation(date1, date2) == 173:
+    print("Pass")
+else:
+    print("error")
+# print(diffCalculation(date1, date2), "days")
+
+# test unit set 3, should return "2036"
+date1 = Date(1, 3, 1989)
+date2 = Date(3, 8, 1983)
+if diffCalculation(date1, date2) == 2036:
+    print("Pass")
+else:
+    print("error")
+# print(diffCalculation(date1, date2), "days")
--- userInput.py
+++ userInput.py
@@ -1,5 +1,64 @@
 #!./my_env/bin/python
 
-from datediff import userInput
+from datediff import *
 
-userInput()
+# prompt and accept user input for first and second date entry with some data validation
+while True:
+    counter = 0
+    if counter == 0:
+        print("Please enter first date:")
+        entry = input().split("/")
+        try:
+            day = int(entry[0])
+            month = int(entry[1])
+            year = int(entry[2])
+            firstdate = Date(day, month, year)
+            counter += 1
+        except:
+            print("Invalid entry, the correct format is dd/mm/yyyy")
+            continue
+        if month != 2:
+            if day > Days_in_Month[month - 1]:
+                print("Please check the date on the month and try enter the correct date.")
+                continue
+        elif month == 2:
+            if not isLeapYear(year):
+                if day > 28:
+                    print("Cannot accept days bigger than 28 if not leap year.")
+                    continue
+            if day > 29:
+                print("There are maximum of 29 days in Feburary")
+                continue
+        if month > 12 or 1900 > year > 2999:
+            print("Invalid date entry, the date should between 01/01/1900 and 31/12/2999")
+            continue
+    if counter == 1:
+        print("Please enter second date:")
+        entry = input().split("/")
+        try:
+            day = int(entry[0])
+            month = int(entry[1])
+            year = int(entry[2])
+            seconddate = Date(day, month, year)
+        except:
+            print("Invalid entry, the correct format is dd/mm/yyyy")
+            continue
+        if month != 2:
+            if day > Days_in_Month[month - 1]:
+                print("Please check the date on the month and try enter the correct date.")
+                continue
+        elif month == 2:
+            if not isLeapYear(year):
+                if day > 28:
+                    print("Cannot accept days bigger than 28 if not leap year.")
+                    continue
+            if day > 29:
+                print("There are maximum of 29 days in Feburary")
+                continue
+        if month > 12 or 1900 > year > 2999:
+            print("invalid date entry, the date should between 01/01/1900 and 31/12/2999")
+            continue
+        
+        break
+
+print(diffCalculation(firstdate, seconddate), "days")
